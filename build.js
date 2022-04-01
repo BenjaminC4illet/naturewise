1e31cff21cedaf518affe12dbe56eb89824574e8
BenjaminC4illet <benjamin.caillet@meisterlabs.com>
2022-04-01 10:40:32 +0200

    Change button color

--- build.js
+++ build.js
@@ -1,3 +1,23 @@
 const StyleDictionary = require('style-dictionary').extend('src/config.js');
 
+const {fileHeader} = StyleDictionary.formatHelpers;
+
+const myCustomFormat = ({ dictionary, file }) => {
+    return `${fileHeader({file, commentStyle: 'short'})}${dictionary.allTokens.map(token => {
+      return `--${token.name}: ${token.value};`
+    }).join(`\n`)}`
+  }
+
+  // Adding a custom file header with the `.registerFileHeader`
+StyleDictionary.registerFileHeader({
+    name: `customFileHeader`,
+    fileHeader: () => {
+      return [
+        `Do not edit directly`,
+        `build hash ${hash}`
+      ]
+    }
+  });
+
+
 StyleDictionary.buildAllPlatforms();
